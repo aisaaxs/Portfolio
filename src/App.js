ce6c996a9e0e39fa50502ba0e78e2438167ffb6a
Anitej Isaac Sharma <anitejsharmas@gmail.com>
2024-07-31 02:06:17 +0530

    added basic home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
+import home_page_light from "./images/Home-Page-Light.png";
+import home_page_dark from "./images/Home-Page-Dark.png";
+
 function App() {
+  const [useDarkMode, setUseDarkMode] = useState(true);
+
   return (
-    <div className="">
+    <div className={`w-screen h-screen ${useDarkMode ? 'bg-[#15191F]' : 'bg-[#f5f5f5]'} flex flex-col`}>
+      <Navbar useDarkMode={useDarkMode} setUseDarkMode={setUseDarkMode} />
       
+      <section className="w-full flex items-center justify-center pt-[50px] absolute top-[70px] left-0">
+        <img src={useDarkMode ? home_page_dark : home_page_light} className="w-full min-w-[1460px]" alt="Home Page" style={{ height: 'calc(100vh - 120px)' }}  />
+      </section>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,83 @@
+import logo from "../../images/logo.png";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
+import { faCodeBranch, faMoon, faSun, faHouse, faUser, faBriefcase, faLaptop, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+const Navbar = ({ useDarkMode, setUseDarkMode }) => {
+    const tooltipClass = useDarkMode ? 'tooltip-dark' : 'tooltip-light';
+
+    const [currentPage, setCurrentPage] = useState("Home Page");
+
+    return (
+        <div className={`w-full min-w-[1460px] h-auto min-h-[70px] grid grid-cols-[40%_auto_5%] ${useDarkMode ? 'bg-[#15191F] border-[#f5f5f5]' : 'bg-[#f5f5f5] border-[#15191F]'} fixed top-0 left-0`}>
+            <div className="flex justify-start items-center">
+                <code className="press-start-2p-regular text-[15px] uppercase m-0 p-0 leading-tight text-black">
+                    <span className="bg-stone-300 py-[10px] px-[10px]">~/W/V/</span>
+                    
+                    <span className="bg-[#fdd615] py-[10px] px-[10px]">Anitej Isaac Sharma</span>
+
+                    <span className="py-[10px] px-[10px] bg-orange-500">
+                        <FontAwesomeIcon icon={faGithubAlt} className="text-[22px]" />
+
+                        <FontAwesomeIcon icon={faCodeBranch} className="text-[22px] ml-[10px]" />
+
+                        <span className="lowercase ml-[10px]">main</span>
+                    </span>
+                </code>
+            </div>
+
+            <div className={`flex justify-evenly items-center press-start-2p-regular ${useDarkMode ? 'text-white' : 'text-black'} flex-row flex-wrap`}>
+                <div className={`tooltip ${tooltipClass}`}>
+                    <FontAwesomeIcon icon={faHouse} className="text-[25px] cursor-pointer" onClick={() => setCurrentPage("Home Page")} />
+                    
+                    <hr className={`w-full h-[4px] bg-orange-500 mt-[4px] border-none ${currentPage == "Home Page" ? 'visible' : 'hidden'}`} />
+                    
+                    <span className="tooltiptext">Home Page</span>
+                </div>
+                
+                <div className={`tooltip ${tooltipClass}`}>
+                    <FontAwesomeIcon icon={faUser} className="text-[25px] cursor-pointer" onClick={() => setCurrentPage("About Me")} />
+                    
+                    <hr className={`w-full h-[4px] bg-orange-500 mt-[4px] border-none ${currentPage == "About Me" ? 'visible' : 'hidden'}`} />
+                    
+                    <span className="tooltiptext">About Me</span>
+                </div>
+                
+                <div className={`tooltip ${tooltipClass}`}>
+                    <FontAwesomeIcon icon={faBriefcase} className="text-[25px] cursor-pointer" onClick={() => setCurrentPage("Professional Experience")} />
+                    
+                    <hr className={`w-full h-[4px] bg-orange-500 mt-[4px] border-none ${currentPage == "Professional Experience" ? 'visible' : 'hidden'}`} />
+                    
+                    <span className="tooltiptext">Professional Experience</span>
+                </div>
+                
+                <div className={`tooltip ${tooltipClass}`}>
+                    <FontAwesomeIcon icon={faLaptop} className="text-[25px] cursor-pointer" onClick={() => setCurrentPage("View Projects")} />
+                    
+                    <hr className={`w-full h-[4px] bg-orange-500 mt-[4px] border-none ${currentPage == "View Projects" ? 'visible' : 'hidden'}`} />
+                    
+                    <span className="tooltiptext">View Projects</span>
+                </div>
+                
+                <div className={`tooltip ${tooltipClass}`}>
+                    <FontAwesomeIcon icon={faEnvelope} className="text-[25px] cursor-pointer" onClick={() => setCurrentPage("Contact Me")} />
+                    
+                    <hr className={`w-full h-[4px] bg-orange-500 mt-[4px] border-none ${currentPage == "Contact Me" ? 'visible' : 'hidden'}`} />
+                    
+                    <span className="tooltiptext">Contact Me</span>
+                </div>
+            </div>
+
+            <div className={`border-l-[2px] border-b-[2px] ${useDarkMode ? 'border-white bg-white' : 'border-black bg-black'} flex justify-center items-center cursor-pointer`} onClick={() => setUseDarkMode(prev => !prev)}>                
+                <FontAwesomeIcon 
+                    icon={useDarkMode ? faSun : faMoon} 
+                    className={`${useDarkMode ? 'text-black' : 'text-white'} text-[25px]`} 
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
